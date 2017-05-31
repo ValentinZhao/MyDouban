aa7dac0eacf4f4b356622c90d290395a642a203b
ValentinZhao <2264709714@qq.com>
2017-05-31 14:33:00 +0800

    first commit

--- src/store/modules/activities.js
+++ src/store/modules/activities.js
@@ -19,3 +19,37 @@ const mutation = {
     state.eventItem = payload.res
   }
 }
+
+const action = {
+  getEvent ({commit}) {
+    Vue.http.jsonp('https://api.douban.com/v2/event/list?loc=108288&count=5')
+              .then(res => {
+                commit({
+                  type: 'getEvent',
+                  res: res.body.events
+                })
+              })
+  },
+  loadMore ({commit, state}) {
+    Vue.http.jsonp('https://api.douban.com/v2/event/list?loc=108288&start=' +
+                      state.skip + '&counts=5')
+              .then(res => {
+                commit({
+                  type: 'loadMore',
+                  res: res.body.events
+                })
+              })
+  },
+  getSingleEvent ({commit, state}, payload) {
+    return new Promise((resolve, reject) => {
+      Vue.http.jsonp('https://api.douban.com/v2/event/' + payload.id)
+              .then(res => {
+                commit({
+                  type: 'getSingleEvent',
+                  res: res.body
+                })
+                resolve(res)
+              })
+    })
+  }
+}
